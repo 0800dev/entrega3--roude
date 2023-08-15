265087f05bb7c81fb329c19a1e1d4acd65afcb1a
federoude <federoude@gmail.com>
2023-08-15 10:50:46 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function saludar (){
+        console.log ("Hi motherfuckers;");
+     };
+     
+     function promedioDeGolLeoEnMls (goles, partidos){
+        let promedioDeGolDeMessiEnLaMls = (goles / partidos);
+        console.log (promedioDeGolDeMessiEnLaMls);
+     };
+     
+// clase n5 de objetos code 
+
+let pokemon = {
+        tipo: "Fuego",
+        nombre: "Charmander",
+        entrenador : "Ash",
+        ataque: function ataquedefuego (intensidad, energia){
+                let poder = 0;
+                if (poder )
+        }
+};
+
+// 
